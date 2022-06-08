6f34a79ab0ecb5ef50ffcf9d971275f0be26ef25
wandini14 <106502763+wandini14@users.noreply.github.com>
2022-06-08 17:26:08 +0800

    Update projectdemo.py

--- projectdemo.py
+++ projectdemo.py
@@ -39,9 +39,8 @@ prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
 df = pd.DataFrame(
-    np.random.randn(3, 1),
+    ['setosa', 'versicolor','virginica']),
     columns=('species')
-
 st.table(df)
 st.write(['setosa', 'versicolor','virginica'])
 
